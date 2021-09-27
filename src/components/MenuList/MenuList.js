a804e66e694f49f5ff3d9e6dea133583394b9e2b
Night mare <hnbtranspkt@gmail.com>
2021-09-28 04:25:26 +0700

    [Fix] Nightmare: Fix mobile background and route

--- src/components/MenuList/MenuList.js
+++ src/components/MenuList/MenuList.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { onTriggerMenu } from 'services/useDevelopUI'
 import './MenuList.css'
 
 export const MenuList = () => {
 	return (
-		<div id={'menu'} className='menu-list'>
+		<div onClick={() => onTriggerMenu()} id={'menu'} className='menu-list'>
 			<div className='list'>
 				<div className='item'>Marketing</div>
 				<div className='item'>Staking</div>
 				<div className='item'>Wallet</div>
+				<Link to='/about'>
+					<div className='item'>About</div>
+				</Link>
 			</div>
 		</div>
 	)
